3951f4ac1135baba960a9632fad437ce15787a54
Ron Kurr <kurr@kurron.org>
2016-02-25 17:11:50 +0000

    Put in  a gateway for the fragement document.

---
+++ src/main/groovy/org/kurron/dcr/DockerComposeFragment.groovy
@@ -0,0 +1,57 @@
+/*
+ * Copyright (c) 2016. Ronald D. Kurr kurr@jvmguy.com
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.kurron.dcr
+
+import groovy.transform.Canonical
+import org.bson.types.ObjectId
+import org.springframework.data.annotation.Id
+import org.springframework.data.mongodb.core.mapping.Document
+
+/**
+ * A MongoDB document that stores individual Docker Compose fragments.
+ **/
+@Document
+@Canonical
+class DockerComposeFragment {
+
+    /**
+     * Primary key of the document.
+     */
+    @Id
+    ObjectId id
+
+    /**
+     * Binary representation of the Docker Compose fragment (YAML does not play well with JSON)
+     */
+    byte[] fragment
+
+    /**
+     * The release this fragment belongs to, eg. "milestone", "release", "rc", etc.
+     */
+    String release
+
+    /**
+     * The version of the fragment, eg module-1.2.3.RELEASE.
+     */
+    String version
+
+    /**
+     * The collection of applications that this fragment is associated with.  For example,
+     * "reporting application" or "firewall".
+     */
+    List<String> applications
+}
---
+++ src/test/groovy/org/kurron/dcr/outbound/DockerComposeFragmentGatewayIntegrationTest.groovy
@@ -0,0 +1,52 @@
+/*
+ * Copyright (c) 2016. Ronald D. Kurr kurr@jvmguy.com
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.kurron.dcr.outbound
+
+import org.kurron.dcr.Application
+import org.kurron.dcr.DockerComposeFragment
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.boot.test.SpringApplicationContextLoader
+import org.springframework.boot.test.WebIntegrationTest
+import org.springframework.test.context.ContextConfiguration
+import spock.lang.Specification
+
+/**
+ * Integration test of the DockerComposeFragmentGateway.
+ */
+@WebIntegrationTest( randomPort = true )
+@ContextConfiguration( classes = Application, loader = SpringApplicationContextLoader )
+class DockerComposeFragmentGatewayIntegrationTest extends Specification {
+
+    @Autowired
+    DockerComposeFragmentGateway sut
+
+    def 'verify crud methods'() {
+        given: 'the gateway was injected'
+        sut
+
+        when: 'we insert a document'
+        def toSave = new DockerComposeFragment( release: 'release',
+                                                version: 'version',
+                                                applications: ['a','b','c'],
+                                                fragment: new byte[8] )
+        DockerComposeFragment saved = sut.save( toSave )
+
+        then: 'we can read it back out'
+        DockerComposeFragment read = sut.findOne( saved.id )
+        read
+    }
+}
